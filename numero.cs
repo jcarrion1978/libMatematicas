1cb850079f5f86bd074476f5c1ae61b2bba281da
jcarrion1978 <jorge.carrion.q@outlook.com>
2022-10-25 14:31:14 -0500

    agregar clase numero con la operación suma

---
+++ numero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	private int num1;
+	private int num2;
+	public Class1()
+	{
+		
+	}
+
+    public int Num1 { get => num1; set => num1 = value; }
+    public int Num2 { get => num2; set => num2 = value; }
+
+	public int suma()
+	{
+		return num1 + num2;
+	}
+}
